870d403ebacec10b575df6e2dcaaa735f328e106
Yean <yeeninw55@gmail.com>
2024-05-12 22:39:13 +0700

    update sending event to topic

--- Self-Learning/Kafka,Multithread/kafkaProducer.py
+++ Self-Learning/Kafka,Multithread/kafkaProducer.py
@@ -5,4 +5,6 @@ producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
 producer.send('test3', json.dumps({'hello': 'world'}).encode('utf-8'))
 producer.send('test2', json.dumps({'hello': 'world'}).encode('utf-8'))
+producer.send('test', json.dumps({'hello': 'world'}).encode('utf-8'))
+
 producer.flush()
